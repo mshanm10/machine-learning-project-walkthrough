238b71d568f149ba92fab54ffa8f019226cb7166
mshanm <muthu@absolut-e.com>
2019-07-17 11:57:10 -0400

    Dockerized.

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM continuumio/anaconda3
+
+#RUN pip install jupyter
+#
+#RUN pip install seaborn
+#
+#RUN pip install ggplot
---
+++ bin/run_notebook.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# --------------------------------------------------------------
+# Run from project root folder
+# ./bin/run_notebook.sh
+# --------------------------------------------------------------
+
+
+docker-compose up --build
